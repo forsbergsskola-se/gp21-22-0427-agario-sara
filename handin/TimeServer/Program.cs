10be53faeac707237240d2cb2e4fea188e23724f
Sara Saber <sarasaberdev@gmail.com>
2023-10-11 20:05:49 +0200

    Send DateTime To Client

--- handin/TimeServer/Program.cs
+++ handin/TimeServer/Program.cs
@@ -21,16 +21,22 @@ public static class Program
         {
             //Accept new client that tries to connect
             TcpClient tcpClient = tcpListener.AcceptTcpClient();
-            byte[] buffer = new byte[100];
             NetworkStream stream = tcpClient.GetStream();
+            //(Optional) Reading from the client
+            byte[] buffer = new byte[100];
             stream.Read(buffer, 0, 100);
+            //Optional: Show what the client said:
             Console.WriteLine("Client said: " + Encoding.ASCII.GetString(buffer));
 
-
-
-
+            //Get the current dateTime:
+            DateTime currentTime = DateTime.Now;
+            string message = "-----Actual Date time:" + "\r\n";
+            message+= currentTime.ToString("yyyy-MM-dd HH:mm:ss") + "\r\n";
+            message += "Thank you for connecting!";
+            //Send the current DateTime Encoded into Bytes
+            byte[] responseBuffer = Encoding.ASCII.GetBytes(message);
+            stream.Write(responseBuffer, 0, responseBuffer.Length);
             
-
         }
     }
 }
