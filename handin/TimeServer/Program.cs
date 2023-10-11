96449fcb187c4a5d609f3b86d534948b6cc7006d
Sara Saber <sarasaberdev@gmail.com>
2023-10-11 19:07:45 +0200

    Opening socket for Time Server

--- handin/TimeServer/Program.cs
+++ handin/TimeServer/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public static class Program
+{
+    static void Main()
+    {
+        //Opening the socket
+        int port = 44444;
+        //IPEndpoint like in the sample TCP server, called it instead of assignment of address abd port ti TcpListener
+        IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, port);
+        TcpListener tcpListener = new TcpListener(endpoint);
+        tcpListener.Start();
+        Console.WriteLine("--------Hello From Server!");
+        Console.WriteLine($"--------Time server running... Listening on port: {port}");
+
+
+    }
+}
