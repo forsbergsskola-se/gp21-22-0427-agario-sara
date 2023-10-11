fda3ba9828270706cf1e1ffb4a907eb255510dbe
Sara Saber <sarasaberdev@gmail.com>
2023-10-12 00:21:30 +0200

    Part 3 -OpenWordMMO - Create server

--- handin/OpenWord-MMO/Program.cs
+++ handin/OpenWord-MMO/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    private const int PORT = 44444;
+    private const byte MAXIMUMWORDLENGTH = 20;
+    private const byte WHITESPACEBYTE = 32;
+
+    private static UdpClient? udpClient;
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("--------Hello From Server!");
+        Console.WriteLine($"--------MMO-Word server running... Listening on port: {PORT}");
+
+        //Initializ the UDP client and bind it to the specified port
+        udpClient = new UdpClient(PORT);
+            
+        //Loop and send a message "accepted" when connection is established
+        while (true)
+        {
+            Console.WriteLine("---||Waiting for connections...");
+
+            //Receive data from a client and get the remote endpoint
+            var (receivedBytes, remoteEndPoint) = ReceiveData();
+
+            Console.WriteLine($"Received {receivedBytes.Length} bytes...");
+
+            if (ValidateReceivedMessage(receivedBytes))
+            {
+                Console.WriteLine("--||Accepted!");
+
+                string responseMessage = Encoding.ASCII.GetString(receivedBytes);
+                SendData(responseMessage, remoteEndPoint);
+            }
+        }
+    }
+
+    //Receive data from a client and get the remote endpoint
+    private static (byte[], IPEndPoint) ReceiveData()
+    {
+        IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        byte[] receivedBytes = udpClient.Receive(ref remoteEndPoint);
+        return (receivedBytes, remoteEndPoint);
+    }
+    
+    //Send data (a response message) to a client at the provided remote endpoint
+
+    private static void SendData(string message, IPEndPoint remoteEndPoint)
+    {
+        byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+        udpClient.Send(messageBytes, messageBytes.Length, remoteEndPoint);
+    }
+
+    //Validate the received message
+    private static bool ValidateReceivedMessage(byte[] receivedBytes)
+    {
+        if (receivedBytes.Length == 0)
+        {
+            SendErrorMessage("//!//Error! Nothing was received!");
+            return false;
+        }
+
+        if (receivedBytes.Length > MAXIMUMWORDLENGTH)
+        {
+            SendErrorMessage($"//!// Error! Sent word is too long! Please limit words to {MAXIMUMWORDLENGTH} letters!");
+            return false;
+        }
+
+        if (Array.IndexOf(receivedBytes, WHITESPACEBYTE) >= 0)
+        {
+            SendErrorMessage("//!//Error! You are only allowed to send one word at a time. Remember that spaces are banned!");
+            return false;
+        }
+
+        return true;
+    }
+
+    //Send a text error message to the client
+
+    private static void SendErrorMessage(string errorMessage)
+    {
+        var errorMessageBytes = Encoding.ASCII.GetBytes(errorMessage);
+        udpClient.Send(errorMessageBytes, errorMessageBytes.Length, new IPEndPoint(IPAddress.Any, 0));
+        Console.WriteLine($"Denied! {errorMessage}");
+    }
+}
